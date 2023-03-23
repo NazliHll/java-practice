fb905b20731fe157b60da9a0a37a6767b458c72f
Nazlı Yaşar <nazlihilalysr@gmail.com>
2023-03-23 11:05:05 +0300

    23.03.2023

---
+++ src/day01_recap/Q02_Println_vs_print.java
@@ -0,0 +1,13 @@
+package day01_recap;
+
+public class Q02_Println_vs_print {
+    public static void main(String[] args) {
+        System.out.println("welcome");
+        System.out.println("java");
+        System.out.println("practice");
+
+        System.out.print("welcome");
+        System.out.print(" java");
+        System.out.print(" class");
+    }
+}
